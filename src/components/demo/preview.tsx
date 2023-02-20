5cdebb7ebc04fe94577d153a539b9910e60319b0
liujiaqing <liujiaqing@mananacare.com>
2023-02-20 11:56:38 +0800

    feat: 修改下模板

--- src/components/demo/preview.tsx
+++ src/components/demo/preview.tsx
@@ -4,6 +4,6 @@ import "./styles.less";
 export type TDemoComponentProps = {};
 
 /* 预览时显示的组件 */
-export const DemoProview: React.FunctionComponent<TDemoComponentProps> = () => {
+export const DemoPreview: React.FunctionComponent<TDemoComponentProps> = () => {
   return <div className={"blue"}>preview 12341234</div>;
 };
