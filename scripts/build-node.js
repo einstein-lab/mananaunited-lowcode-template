44cfcbfc17b6c609c35863a81d0e288130ddcf68
liujiaqing <liujiaqing@mananacare.com>
2023-03-13 14:11:39 +0800

    feat: 修改脚本

--- scripts/build-node.js
+++ scripts/build-node.js
@@ -1,12 +1,17 @@
 const webpack = require("webpack");
 // 读取 webpack.config.js 文件中的配置
 const configFn = require("../config/webpack.prod.config");
+const chalk = require("react-dev-utils/chalk");
 const config = configFn();
 
 console.log("Creating an optimized production build...");
-webpack(config).run((error, state) => {
-  if (error) {
-    console.error(error);
+webpack(config).run((_, state) => {
+  const errors = state?.compilation?.errors;
+  if (errors?.length) {
+    for (const item of errors) {
+      console.log(chalk.red(item?.message));
+    }
+    return console.log(chalk.red("build failed"));
   }
-  console.log("build success");
+  console.log(chalk.green("build success"));
 });
